c366799b3f55c419a4c8a558c2a18d57e888c86e
nenad-misic <33166172+nenad-misic@users.noreply.github.com>
2021-06-28 23:40:32 +0200

    Mute (#33)
    * First milestone (#29)
    
    * Devops (#22)
    
    * Add docker image (Dockerfile) #1
    
    * Add docker-compose and package.json
    
    * Terminate issue closes #1
    
    * Add login functionality closes #3 (#23)
    
    * Add registration page and test it out (manually) closes #2 (#24)
    
    * Implement post creation functionality closes #5 (#25)
    
    * Add post page closes #4 (#26)
    
    * Add profile page closes #6 (#27)
    
    * Follow req (#28)
    
    * Add follow requests and test it out
    
    * Add follow requests and test it out closes #9
    
    * Add mute functionality closes #31

--- src/Services/userService.js
+++ src/Services/userService.js
@@ -19,6 +19,10 @@ let userService = {
         let url = `${api}user/api/follow`;
         return axios.post(url, {dst: id, mute: false}, bearer());
     },
+    mute: function(id) {
+        let url = `${api}user/api/follow/mute/${id}`;
+        return axios.get(url, bearer());
+    },
     login: async function (username, password) {
         let url = `${api}user/api/login`;
         try {
